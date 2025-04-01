0aaaed0cdd51eba5e1d4b436f33ed4ca4d283a69
David Millman <dave@cs.unc.edu>
2025-03-31 19:16:05 -0600

    Add go-internal package

---
+++ nix/go-internal.nix
@@ -0,0 +1,24 @@
+{
+  pkgs,
+}:
+pkgs.buildGoModule rec {
+  pname = "go-internal";
+  version = "1.14.1";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "rogpeppe";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-6NzhXCCD1Qhj05WHbCDxH5hwfNM6psoAk7uIxm7N55E=";
+  };
+
+  vendorHash = "sha256-WoRmZbYYpwVVetlxJDjUu9jGgwLXUD3/PnUF6ksUT70=";
+  doCheck = false;
+
+  meta = {
+    description = "The testscript command Runs testscript scripts in a fresh temporary work directory tree";
+    homepage = "https://github.com/rogpeppe/go-internal/";
+    license = pkgs.lib.licenses.bsd3;
+    maintainers = with pkgs.lib.maintainers; [ dlm ];
+  };
+}
