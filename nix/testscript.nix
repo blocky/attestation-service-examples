bf7690ead5f940d3e3632342a76909b6584a9091
David Millman <dave@cs.unc.edu>
2025-04-02 10:35:20 -0600

    Install testscript only

--- nix/testscript.nix
+++ nix/testscript.nix
@@ -2,22 +2,24 @@
   pkgs,
 }:
 pkgs.buildGoModule rec {
-  pname = "go-internal";
+  pname = "testscript";
   version = "1.14.1";
 
   src = pkgs.fetchFromGitHub {
     owner = "rogpeppe";
-    repo = pname;
+    repo = "go-internal";
     rev = "v${version}";
     hash = "sha256-6NzhXCCD1Qhj05WHbCDxH5hwfNM6psoAk7uIxm7N55E=";
   };
 
   vendorHash = "sha256-WoRmZbYYpwVVetlxJDjUu9jGgwLXUD3/PnUF6ksUT70=";
+
   doCheck = false;
+  subPackages = [ "./cmd/testscript" ];
 
   meta = {
-    description = "The testscript command Runs testscript scripts in a fresh temporary work directory tree";
-    homepage = "https://github.com/rogpeppe/go-internal/";
+    description = "The testscript command runs testscript scripts in a fresh temporary work directory tree";
+    homepage = "https://github.com/rogpeppe/go-internal/cmd/testscript";
     license = pkgs.lib.licenses.bsd3;
     maintainers = with pkgs.lib.maintainers; [ dlm ];
   };
