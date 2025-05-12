1ae420f1b0084539165083a0aa4bd8dc811d2485
Matteo Bjornsson <matteo@blocky.rocks>
2025-05-12 14:29:40 -0600

    specify both live and local test

--- test/coin_prices_from_coingecko_test.go
+++ test/coin_prices_from_coingecko_test.go
@@ -7,13 +7,46 @@ import (
 
 const examplesDir = ".."
 
+const configTemplate = `
+acceptable_measurements = [
+{ platform = "{{.LIVE_TEST_PLATFORM}}", code = "{{.LIVE_TEST_CODE}}" },
+]
+auth_token = "{{.LIVE_TEST_AUTH_TOKEN}}"
+host = "{{.LIVE_TEST_HOST}}"
+`
+
 func TestCoinPricesFromCoingecko(t *testing.T) {
-	projectName := "coin_prices_from_coingecko"
-	projectDir := filepath.Join(examplesDir, projectName)
-	newProjectTest(t, projectDir).
-		ExecuteMakeTarget("build").
-		CopyFile("tmp/x.wasm").
-		CopyFile("config.toml").
-		RenderTemplateFromEnvWithCleanup("fn-call.json", []string{"YOUR_COINGECKO_API_KEY"}).
-		RunScript(filepath.Join(".", "scripts", projectName+".txtar"))
+	t.Run("coingecko example local test", func(t *testing.T) {
+		projectName := "coin_prices_from_coingecko"
+		projectDir := filepath.Join(examplesDir, projectName)
+		newProjectTest(t, projectDir).
+			ExecuteMakeTarget("build").
+			CopyFile("tmp/x.wasm").
+			CopyFile("config.toml").
+			RenderTemplateFileFromEnvWithCleanup(
+				"fn-call.json",
+				[]string{"YOUR_COINGECKO_API_KEY"}).
+			RunScript(filepath.Join(".", "scripts", projectName+".txtar"))
+	})
+
+	t.Run("coingecko example live test", func(t *testing.T) {
+		projectName := "coin_prices_from_coingecko"
+		projectDir := filepath.Join(examplesDir, projectName)
+		newProjectTest(t, projectDir).
+			ExecuteMakeTarget("build").
+			CopyFile("tmp/x.wasm").
+			RenderTemplateFileFromEnvWithCleanup(
+				"fn-call.json",
+				[]string{"YOUR_COINGECKO_API_KEY"}).
+			RenderTemplateStringFromEnvWithCleanup(
+				configTemplate,
+				"config.toml",
+				[]string{
+					"LIVE_TEST_PLATFORM",
+					"LIVE_TEST_CODE",
+					"LIVE_TEST_AUTH_TOKEN",
+					"LIVE_TEST_HOST",
+				}).
+			RunScript(filepath.Join(".", "scripts", projectName+".txtar"))
+	})
 }
