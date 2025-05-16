718667c5bcfb6c8fa09e3ca8987c8da549fbad09
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2025-05-16 07:23:03 -0400

    Add testscript test for esports_data_from_pandascore example

--- test/examples_test.go
+++ test/examples_test.go
@@ -36,6 +36,21 @@ func TestErrorHandlingAttestFnCall(t *testing.T) {
 		RunScript(filepath.Join(scriptDir, projectName+".txtar"))
 }
 
+func TestESportsDataFromPandaScore(t *testing.T) {
+	projectName := "esports_data_from_pandascore"
+	projectDir := filepath.Join(examplesDir, projectName)
+	requiredEnvVars := []string{
+		"YOUR_PANDASCORE_API_KEY",
+	}
+
+	NewProjectTest(t, projectDir).
+		ExecuteMakeTarget("build").
+		CopyFile("tmp/x.wasm").
+		CopyFile("config.toml").
+		RenderTemplateFileFromEnvWithCleanup("fn-call.json", requiredEnvVars).
+		RunScript(filepath.Join(scriptDir, projectName+".txtar"))
+}
+
 func TestHelloWorldAttestFnCall(t *testing.T) {
 	projectName := "hello_world_attest_fn_call"
 	projectDir := filepath.Join(examplesDir, projectName)
