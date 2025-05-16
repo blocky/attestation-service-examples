b3e4d2de61406242b07e18e43d507403084f955a
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2025-05-16 06:10:34 -0400

    Add testscript test for error_handling_attest_fn_call example

--- test/examples_test.go
+++ test/examples_test.go
@@ -23,6 +23,19 @@ func TestCoinPricesFromCoingecko(t *testing.T) {
 		RunScript(filepath.Join(scriptDir, projectName+".txtar"))
 }
 
+func TestErrorHandlingAttestFnCall(t *testing.T) {
+	projectName := "error_handling_attest_fn_call"
+	projectDir := filepath.Join(examplesDir, projectName)
+	NewProjectTest(t, projectDir).
+		ExecuteMakeTarget("build").
+		CopyFile("tmp/x.wasm").
+		CopyFile("config.toml").
+		CopyFile("fn-call.json").
+		CopyFile("fn-call-error.json").
+		CopyFile("fn-call-panic.json").
+		RunScript(filepath.Join(scriptDir, projectName+".txtar"))
+}
+
 func TestHelloWorldAttestFnCall(t *testing.T) {
 	projectName := "hello_world_attest_fn_call"
 	projectDir := filepath.Join(examplesDir, projectName)
