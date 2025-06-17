f10913b58ffc7cb4125ef82f30f4d68c069c8fbe
Taylor Hardin <Taylor.Antonio.Hardin@gmail.com>
2025-06-17 12:13:54 -0400

    [BKy-6882] remove sepolia tests (#60)
    * Remove live Sepolia test and associated helper funcs and resources
    
    * Remove extra newlines
    
    * Remove live Sepolia test and associated helper funcs and resources
    
    * Run npm audit fix
    
    * Remove mention of Sepolia tests and fix grammar mistake
    
    * Remove mention of Sepolia tests

--- error_handling_on_chain/hardhat.config.ts
+++ error_handling_on_chain/hardhat.config.ts
@@ -6,11 +6,6 @@ require('dotenv').config();
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    'base-sepolia': {
-      url: 'https://sepolia.base.org',
-      accounts: [process.env.WALLET_KEY as string],
-      gasPrice: 1000000000,
-    },
     'base-local': {
       url: 'http://localhost:8545',
       accounts: [process.env.WALLET_KEY as string],
@@ -18,21 +13,6 @@ const config: HardhatUserConfig = {
     },
   },
   defaultNetwork: 'hardhat',
-  etherscan: {
-    apiKey: {
-      "base-sepolia": process.env.BASESCAN_KEY as string,
-    },
-    customChains: [
-      {
-        network: "base-sepolia",
-        chainId: 84532,
-        urls: {
-          apiURL: "https://api-sepolia.basescan.org/api",
-          browserURL: "https://sepolia.basescan.org"
-        }
-      },
-    ]
-  },
 };
 
 export default config;
