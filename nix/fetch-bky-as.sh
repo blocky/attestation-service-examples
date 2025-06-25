0a281024b51b634a75c63fb5fdf0d6a0cb400dae
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2025-06-25 08:37:31 -0400

    Try setting AWS env vars and add a check for their existence in the fetch-bky-as script

--- nix/fetch-bky-as.sh
+++ nix/fetch-bky-as.sh
@@ -19,9 +19,14 @@ else
 fi
 
 if [[ $commit == "latest" ]]; then
-  echo -n "Getting the latest commit..."
-  commit=$(gh api repos/blocky/delphi/commits --jq '.[0].sha')
-  echo "'$commit'"
+    echo -n "Getting the latest commit..."
+    commit=$(gh api repos/blocky/delphi/commits --jq '.[0].sha')
+    echo "'$commit'"
+fi
+
+if [[ -z $AWS_ACCESS_KEY_ID || -z $AWS_SECRET_ACCESS_KEY || -z $AWS_SESSION_TOKEN ]]; then
+    echo "Error: AWS credentials are missing!"
+    exit 1
 fi
 
 if [[ $current_version_commit != "$commit" ]]; then
