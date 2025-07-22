80da487ffec56c09fef796d159cabdb50d941c58
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2025-07-22 09:18:46 -0400

    Add ability to set environment variables for hardhat tests and add option for passing in a different TA input file

--- test/hardhat.go
+++ test/hardhat.go
@@ -11,12 +11,14 @@ type HardhatTest struct {
 	t          *testing.T
 	setupFuncs []func() error
 	projectDir string
+	prevEnv    map[string]string
 }
 
 func NewHardhatTest(t *testing.T, projectDir string) *HardhatTest {
 	return &HardhatTest{
 		t:          t,
 		projectDir: projectDir,
+		prevEnv:    make(map[string]string),
 	}
 }
 
@@ -36,7 +38,17 @@ func (h *HardhatTest) NPMInstall() *HardhatTest {
 	return h
 }
 
+func (h *HardhatTest) SetEnv(key string, value string) *HardhatTest {
+	h.prevEnv[key] = os.Getenv(key)
+	err := os.Setenv(key, value)
+	if err != nil {
+		h.t.Fatalf("failed to set environment variable %s: %v", key, err)
+	}
+	return h
+}
+
 func (h *HardhatTest) Run(args ...string) {
+	defer resetEnv(h)
 	for _, setupFunc := range h.setupFuncs {
 		if err := setupFunc(); err != nil {
 			h.t.Fatalf("failed to setup test: %v", err)
@@ -54,5 +66,14 @@ func (h *HardhatTest) Run(args ...string) {
 	h.t.Logf("Running command: '%s'", cmd.String())
 	if err := cmd.Run(); err != nil {
 		h.t.Fatalf("failed to run hardhat test: %v", err)
+	}
+}
+
+func resetEnv(h *HardhatTest) {
+	for key, value := range h.prevEnv {
+		err := os.Setenv(key, value)
+		if err != nil {
+			h.t.Logf("failed to reset environment variable %s: %v", key, err)
+		}
 	}
 }
